d6a05c780bf627a8094f2b563309374f674fdb37
Daryl V <daryl.villalobos@hotmail.com>
2024-09-27 12:01:09 +0900

    prevent double reply

--- Code.js
+++ Code.js
@@ -3,32 +3,49 @@ function doGet() {
 }
 
 function autoReply(customMessage) {
-  if (typeof customMessage == 'undefined') {
+  if (typeof customMessage == "undefined") {
     customMessage = null;
   }
   const today = new Date();
 
   const myEmail = Session.getActiveUser().getEmail();
   console.log("My email:", myEmail);
-  const todayDayPatterns = getDayDate_(today)
-  console.log(todayDayPatterns)
+  const todayDayPatterns = getDayDate_(today);
+  console.log(todayDayPatterns);
   const checkinEmailThread = findCheckinEmail_(todayDayPatterns);
   const reply_message = customMessage || getGreetingBasedOnTime_();
 
   if (checkinEmailThread) {
+    // Check if script has already replied to this thread
+    const hasReplied = replyToThread_(
+      checkinEmailThread,
+      myEmail,
+      reply_message,
+    );
+    if (hasReplied === "This script replied earlier") {
+      return hasReplied;
+    }
+
     chatCheckin(reply_message);
-    replyToThread_(checkinEmailThread, myEmail, reply_message);
+    return "Replied and posted to chat.";
   } else {
     return "Check-in email thread not found.";
   }
 }
 
-function getDayDate_(today){
+function getDayDate_(today) {
   // today = new Date()
-  const dayLong = today.toLocaleString('default', { weekday: 'long' }).toLowerCase(); // pattern 1
-  const monthSlashDate_num = today.toLocaleString('en-US', { month: 'numeric', day: 'numeric' }); // pattern 2
+  const dayLong = today
+    .toLocaleString("default", { weekday: "long" })
+    .toLowerCase(); // pattern 1
+  const monthSlashDate_num = today.toLocaleString("en-US", {
+    month: "numeric",
+    day: "numeric",
+  }); // pattern 2
   const dayLongSpaceDateSlash_num = `${dayLong} ${monthSlashDate_num}`; // pattern 3
-  const monthLongDateNum = today.toLocaleString('en-US', { month: 'long', day: 'numeric'}).toLowerCase();
+  const monthLongDateNum = today
+    .toLocaleString("en-US", { month: "long", day: "numeric" })
+    .toLowerCase();
   const dayLongCommaMonthLongDateNum = `${dayLong}, ${monthLongDateNum}`; // pattern 4
   const dayLongMonthLongDateNum = `${dayLong}, ${monthLongDateNum}`; // pattern 5
   console.log(`
@@ -37,8 +54,14 @@ function getDayDate_(today){
   pattern 3: ${dayLongSpaceDateSlash_num}
   pattern 4: ${dayLongCommaMonthLongDateNum}
   pattern 5: ${dayLongMonthLongDateNum}
-  `)
-  return [dayLong, monthSlashDate_num, dayLongSpaceDateSlash_num, dayLongCommaMonthLongDateNum, dayLongMonthLongDateNum]
+  `);
+  return [
+    dayLong,
+    monthSlashDate_num,
+    dayLongSpaceDateSlash_num,
+    dayLongCommaMonthLongDateNum,
+    dayLongMonthLongDateNum,
+  ];
 }
 
 function getGreetingBasedOnTime_() {
